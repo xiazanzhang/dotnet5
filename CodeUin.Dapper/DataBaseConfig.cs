6ee6e3a8ff2f66ac603e0072c24790cfe57c79e1
夏赞长 <xiazanzhang@wshifu.com>
2021-01-14 11:34:34 +0800

    commit

--- CodeUin.Dapper/DataBaseConfig.cs
+++ CodeUin.Dapper/DataBaseConfig.cs
@@ -5,7 +5,7 @@ namespace CodeUin.Dapper
 {
     public class DataBaseConfig
     {
-        private static string MySqlConnectionString = @"Data Source=118.89.22.105;Initial Catalog=codeuin;Charset=utf8mb4;User ID=root;Password=Xzz306556182;";
+        private static string MySqlConnectionString = @"Data Source=127.0.0.1;Initial Catalog=codeuin;Charset=utf8mb4;User ID=root;Password=123456789;";
         public static IDbConnection GetMySqlConnection(string sqlConnectionString = null)
         {
             if (string.IsNullOrWhiteSpace(sqlConnectionString))
